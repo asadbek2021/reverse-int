62f173be68dcde7cfe2475670eebc6e12c796cb5
raimovasad <raimovasadbek18@gmail.com>
2022-01-06 01:12:22 +0500

    feat: add funtion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports =function reverse (n) {
+    let arr = String(n).split('');
+    if(n<0){
+        arr.shift()
+    }
+    let bon = arr.reverse()
+   
+    let result = Number(bon.join(''));
+    return result;
 }
+
